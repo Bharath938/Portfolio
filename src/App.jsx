7a81bdb11a365d86ca82da4326205d75ff8216c7
Bharath <bharathkumarkarrolla@gmail.com>
2026-05-17 21:30:09 +0530

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,6 @@ function App() {
       <Services />
       <Projects />
       <Contact />
-      <About />
-      <Skills />
       <Footer />
     </main>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 
 function Footer() {
-  return <div>Footer</div>;
+  const socialLinks = [
+    { icon: <FaGithub />, link: "" },
+    { icon: <FaTwitter />, link: "" },
+    { icon: <FaLinkedin />, link: "" },
+  ];
+  return (
+    <div className="flex justify-around items-center mt-32 bg-[#1a1a2e] p-4">
+      <div className="flex gap-4 items-center">
+        {socialLinks?.map((socialLink) => (
+          <div>
+            <span className="text-green-500 text-2xl">{socialLink.icon}</span>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center text-green-500">
+        <FaRegCopyright />
+        <span className="text-sm">2026 Bharath. All Rights Reserved</span>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
