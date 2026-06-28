ba1e164d73ebd130a8b30e085a8899248dfd2001
Bharath <bharathkumarkarrolla@gmail.com>
2026-06-28 11:37:55 +0530

    Add project image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaArrowAltCircleRight } from "react-icons/fa";
+import project_img from "../assets/image_project.webp"
 
 function Projects() {
   const projects = [
@@ -30,7 +31,7 @@ function Projects() {
         <div className="flex flex-col justify-center gap-4 items-center md:flex-row mt-8">
           {projects?.map((project) => (
             <div className="flex flex-col space-y-2 p-8 rounded-2xl bg-[#1a1a2e]">
-              <span className="text-6xl">{project.image}</span>
+              <img src={project_img} alt="project_img" className="h-32 w-32 object-contain" />
               <span className="text-2xl font-bold">{project.title}</span>
               <span className="text-sm">{project.description}</span>
               <div className="flex gap-2 items-center mt-4 cursor-pointer">
